d6052d87657589d6c57d656f680c472c84dec24a
Julia <>
2021-07-23 17:20:56 +0200

    first commit

---
+++ data/lineDataOverall.js
@@ -0,0 +1,254 @@
+var linedDataOverall = [
+    {
+      "year": 2018,
+      "month": "Jan",
+      "year_month": "2018-01-01",
+      "year_month_id": 1,
+      "value": 95
+    },
+    {
+      "year": 2018,
+      "month": "Feb",
+      "year_month": "2018-02-01",
+      "year_month_id": 2,
+      "value": 74
+    },
+    {
+      "year": 2018,
+      "month": "Mar",
+      "year_month": "2018-03-01",
+      "year_month_id": 3,
+      "value": 58
+    },
+    {
+      "year": 2018,
+      "month": "Apr",
+      "year_month": "2018-04-01",
+      "year_month_id": 4,
+      "value": 46
+    },
+    {
+      "year": 2018,
+      "month": "May",
+      "year_month": "2018-05-01",
+      "year_month_id": 5,
+      "value": 42
+    },
+    {
+      "year": 2018,
+      "month": "Jun",
+      "year_month": "2018-06-01",
+      "year_month_id": 6,
+      "value": 41
+    },
+    {
+      "year": 2018,
+      "month": "Jul",
+      "year_month": "2018-07-01",
+      "year_month_id": 7,
+      "value": 39
+    },
+    {
+      "year": 2018,
+      "month": "Aug",
+      "year_month": "2018-08-01",
+      "year_month_id": 8,
+      "value": 32
+    },
+    {
+      "year": 2018,
+      "month": "Sep",
+      "year_month": "2018-09-01",
+      "year_month_id": 9,
+      "value": 35
+    },
+    {
+      "year": 2018,
+      "month": "Oct",
+      "year_month": "2018-10-01",
+      "year_month_id": 10,
+      "value": 46
+    },
+    {
+      "year": 2018,
+      "month": "Nov",
+      "year_month": "2018-11-01",
+      "year_month_id": 11,
+      "value": 70
+    },
+    {
+      "year": 2018,
+      "month": "Dec",
+      "year_month": "2018-12-01",
+      "year_month_id": 12,
+      "value": 80
+    },
+    {
+      "year": 2019,
+      "month": "Jan",
+      "year_month": "2019-01-01",
+      "year_month_id": 13,
+      "value": 89
+    },
+    {
+      "year": 2019,
+      "month": "Feb",
+      "year_month": "2019-02-01",
+      "year_month_id": 14,
+      "value": 67
+    },
+    {
+      "year": 2019,
+      "month": "Mar",
+      "year_month": "2019-03-01",
+      "year_month_id": 15,
+      "value": 48
+    },
+    {
+      "year": 2019,
+      "month": "Apr",
+      "year_month": "2019-04-01",
+      "year_month_id": 16,
+      "value": 39
+    },
+    {
+      "year": 2019,
+      "month": "May",
+      "year_month": "2019-05-01",
+      "year_month_id": 17,
+      "value": 45
+    },
+    {
+      "year": 2019,
+      "month": "Jun",
+      "year_month": "2019-06-01",
+      "year_month_id": 18,
+      "value": 37
+    },
+    {
+      "year": 2019,
+      "month": "Jul",
+      "year_month": "2019-07-01",
+      "year_month_id": 19,
+      "value": 34
+    },
+    {
+      "year": 2019,
+      "month": "Aug",
+      "year_month": "2019-08-01",
+      "year_month_id": 20,
+      "value": 33
+    },
+    {
+      "year": 2019,
+      "month": "Sep",
+      "year_month": "2019-09-01",
+      "year_month_id": 21,
+      "value": 38
+    },
+    {
+      "year": 2019,
+      "month": "Oct",
+      "year_month": "2019-10-01",
+      "year_month_id": 22,
+      "value": 50
+    },
+    {
+      "year": 2019,
+      "month": "Nov",
+      "year_month": "2019-11-01",
+      "year_month_id": 23,
+      "value": 71
+    },
+    {
+      "year": 2019,
+      "month": "Dec",
+      "year_month": "2019-12-01",
+      "year_month_id": 24,
+      "value": 87
+    },
+    {
+      "year": 2020,
+      "month": "Jan",
+      "year_month": "2020-01-01",
+      "year_month_id": 25,
+      "value": 79
+    },
+    {
+      "year": 2020,
+      "month": "Feb",
+      "year_month": "2020-02-01",
+      "year_month_id": 26,
+      "value": 63
+    },
+    {
+      "year": 2020,
+      "month": "Mar",
+      "year_month": "2020-03-01",
+      "year_month_id": 27,
+      "value": 42
+    },
+    {
+      "year": 2020,
+      "month": "Apr",
+      "year_month": "2020-04-01",
+      "year_month_id": 28,
+      "value": 31
+    },
+    {
+      "year": 2020,
+      "month": "May",
+      "year_month": "2020-05-01",
+      "year_month_id": 29,
+      "value": 30
+    },
+    {
+      "year": 2020,
+      "month": "Jun",
+      "year_month": "2020-06-01",
+      "year_month_id": 30,
+      "value": 29
+    },
+    {
+      "year": 2020,
+      "month": "Jul",
+      "year_month": "2020-07-01",
+      "year_month_id": 31,
+      "value": 26
+    },
+    {
+      "year": 2020,
+      "month": "Aug",
+      "year_month": "2020-08-01",
+      "year_month_id": 32,
+      "value": 25
+    },
+    {
+      "year": 2020,
+      "month": "Sep",
+      "year_month": "2020-09-01",
+      "year_month_id": 33,
+      "value": 29
+    },
+    {
+      "year": 2020,
+      "month": "Oct",
+      "year_month": "2020-10-01",
+      "year_month_id": 34,
+      "value": 44
+    },
+    {
+      "year": 2020,
+      "month": "Nov",
+      "year_month": "2020-11-01",
+      "year_month_id": 35,
+      "value": 70
+    },
+    {
+      "year": 2020,
+      "month": "Dec",
+      "year_month": "2020-12-01",
+      "year_month_id": 36,
+      "value": 90
+    }
+  ]
